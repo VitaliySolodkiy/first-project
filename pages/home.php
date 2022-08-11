5cb89c361529ea039f1b1598a2c62ec600699b06
VitaliySolodkiy <majestis777@gmail.com>
2022-08-11 18:55:17 +0300

    read/write files in home.php

--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,18 @@
 <h1>Home Page</h1>
+
+
+
+<?php
+// $f = fopen('test.txt', 'r+'); //open file. if choose 'r+' and file doesn't exist, return false. if choose 'w' or 'w+' not existing file will be created
+// fwrite($f, '89'); //write text in file
+// fclose($f); //close file
+
+// $text = file_get_contents('test.txt');
+// $text = str_replace("\r\n", "<br>", $text);
+// echo $text;
+
+// $lines = file('test.txt'); //move lines of txt file into array
+// dump($lines);
+
+
+?>
